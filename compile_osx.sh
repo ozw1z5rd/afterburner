8249157ac525bf38bf4c2f46afd9a2584b3a746c
ozw1z5rd <ozw1z5rd@icloud.com>
2024-03-24 20:14:24 +0100

    use the clang shipped with the developer tools

--- compile_osx.sh
+++ compile_osx.sh
@@ -1,7 +1,7 @@
 # path to your Osx cross-compiler
-CC=~/opt/osxcross/bin/o64-clang
+CC=clang
 
 GCOM=`git  rev-parse --short HEAD`
 
 
-$CC -g3 -O0 -DNO_CLOSE -DGCOM="\"g${GCOM}\"" -o afterburner_osx  src_pc/afterburner.c
+$CC -g3 -O0 -DNO_CLOSE -DGCOM="\"g${GCOM}\"" -o afterburner  src_pc/afterburner.c
